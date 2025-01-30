5e2830fa9b5d035abea55dc2dd84d8f919616e60
Alex <aldrake87@gmail.com>
2025-01-30 15:56:28 +0000

    initial set-up and structure

---
+++ src/collisions/data_processor.py
@@ -0,0 +1,6 @@
+import datetime
+
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
+from sklearn.model_selection import train_test_split
