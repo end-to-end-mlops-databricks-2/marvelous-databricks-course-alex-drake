1a56d8f214105ce7a3a92856fbaf466de326130c
Alex <aldrake87@gmail.com>
2025-01-31 16:55:22 +0000

    added tests

---
+++ tests/integration/reservations/test_data_processor.py
@@ -0,0 +1,61 @@
+from pandas import CategoricalDtype
+from pandas.api.types import is_numeric_dtype
+from tests.base_test import BaseTest
+
+from src.reservations.data_processor import DataProcessor
+
+
+class TestDataProcessor(BaseTest):
+    """
+    Test DataProcessor functionality
+    """
+
+    def test_data_processor_loads_df(self):
+        """
+        Test that the data processor loads a dataframe from the 
+        config file when input dataframe is set to None
+        """
+        data_processor = DataProcessor(None, config=self.config)
+        self.assertIsNotNone(
+            data_processor.df,
+            "Imported dataframe should not be None"
+            )
+        
+    def test_preprocessor(self):
+        """
+        Test that the preprocessor handles the data correctly
+        """
+        data_processor = DataProcessor(None, config=self.config)
+        data_processor.preprocess()
+        
+        a_num_feature = self.config.num_features[0]
+        a_cat_feature = self.config.cat_features[0]
+        
+        self.assertTrue(
+            is_numeric_dtype(data_processor.df[a_num_feature]),
+            f"Expected numeric dtype for column {a_num_feature}"
+        )
+        
+        self.assertIsInstance(
+            data_processor.df[a_cat_feature].dtype,
+            CategoricalDtype,
+            f"Expected categorical dtype for column {a_cat_feature}"
+        )
+
+    def test_data_splitter(self):
+        """Test that the dataframe can be split into train and test
+        dataframes.
+        """
+        data_processor = DataProcessor(None, config=self.config)
+        data_processor.preprocess()
+        data_processor.split_data(test_size=0.3, random_state=10)
+
+        self.assertIsNotNone(
+            data_processor.train_df,
+            "Training data should not be None. Check the preprocessing steps."
+        )
+
+        self.assertIsNotNone(
+            data_processor.test_df,
+            "Test data should not be None. Check the preprocessing steps."
+        )
---
+++ tests/unit/reservations/test_config.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from src.reservations.config import Config
+
+
+class TestConfig(TestCase):
+    """Test Config
+    
+    Config is an important part of the project, with
+    other functions and modules depending on it
+    """
+
+    def test_from_yaml(self):
+        """
+        Test config can be opened from YAML
+        """
+        config = Config.from_yaml(
+            "project_config.yml"
+        )
+
+        self.assertEqual(type(config.cat_features), list)
+        self.assertEqual(type(config.num_features), list)
