7ba530b454aa0f5816427e883e3a8ccdd15a9dcd
Alex <aldrake87@gmail.com>
2025-02-20 14:53:35 +0000

    added config for pipeline_id and env extraction

--- src/reservations/config.py
+++ src/reservations/config.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import yaml
 from pydantic import BaseModel
@@ -19,16 +19,28 @@ class Config(BaseModel):
     experiment_name_fe: str
     endpoint_name: str
     parameters: Dict[str, Any]
+    pipeline_id: Optional[str]
 
     @classmethod
-    def from_yaml(cls, config_path: str):
+    def from_yaml(cls, config_path: str, env: str = None):
         """Load config from YAML file"""
 
         with open(config_path, "r") as f:
             config_dict = yaml.safe_load(f)
+
+        if env is not None:
+            config_dict["catalog_name"] = config_dict[env]["catalog_name"]
+            config_dict["schema_name"] = config_dict[env]["schema_name"]
+            config_dict["pipeline_id"] = config_dict[env]["pipeline_id"]
+        else:
+            config_dict["catalog_name"] = config_dict["catalog_name"]
+            config_dict["schema_name"] = config_dict["schema_name"]
+            config_dict["pipeline_id"] = config_dict["pipeline_id"]
+
         return cls(**config_dict)
 
 
 class Tags(BaseModel):
     git_sha: str
     branch: str
+    job_run_id: str
